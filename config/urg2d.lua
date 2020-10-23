4af0ac9e602465841f664ead579175b5878d5613
TaiyouKomazawa <e1611216@toyama.kosen-ac.jp>
2020-10-23 17:23:48 +0900

    cartographerのパラメータを調整。センサをurgからust-10lxに変更

--- config/urg2d.lua
+++ config/urg2d.lua
@@ -15,7 +15,7 @@ options = {
   use_landmarks = false,
   num_laser_scans = 1,
   num_multi_echo_laser_scans = 0,
-  num_subdivisions_per_laser_scan = 1,
+  num_subdivisions_per_laser_scan = 10,
   num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
@@ -32,6 +32,8 @@ MAP_BUILDER.use_trajectory_builder_2d = true
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 2
 TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.submaps.num_range_data = 45
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = false
+TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)  
 
 -- more points
 TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_length = 0.2
@@ -40,15 +42,17 @@ TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.min_num_points = 400
 TRAJECTORY_BUILDER_2D.submaps.range_data_inserter.probability_grid_range_data_inserter.hit_probability = 0.53
 TRAJECTORY_BUILDER_2D.submaps.range_data_inserter.probability_grid_range_data_inserter.miss_probability = 0.493
 -- slightly shorter rays
-TRAJECTORY_BUILDER_2D.max_range = 4.
+TRAJECTORY_BUILDER_2D.min_range = 0.02
+TRAJECTORY_BUILDER_2D.max_range = 4.0
 -- wheel odometry is fine
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 20
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 50
 -- IMU is ok
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 20
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 100
 
 -- less outliers
 POSE_GRAPH.constraint_builder.max_constraint_distance = 5.
-POSE_GRAPH.constraint_builder.min_score = 0.5
+POSE_GRAPH.optimization_problem.huber_scale = 1e2
+POSE_GRAPH.constraint_builder.min_score = 0.8
 -- tune down IMU in optimization
 POSE_GRAPH.optimization_problem.acceleration_weight = 0.1 * 1e3
 POSE_GRAPH.optimization_problem.rotation_weight = 0.1 * 3e5
